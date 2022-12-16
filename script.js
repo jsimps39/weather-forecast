28d44f8efd826f57f7d2472cc816b53115cf7d2c
James S <jamests1000@gmail.com>
2022-12-15 21:05:05 -0500

    worked on display forecast and api url

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var getWeather = function () {
     var city = $('#city').val();
     localStorage.setItem("city", city);
-    var apiUrl = 'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}'// + city
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=0d9f9d9d5f240e636fa9c060806537f3'
     fetch(apiUrl)
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
-                    var forecast = data.forecast;
-                    displayForecast(forecast);
+                    displayForecast(data);
+                    console.log(data);
                 });
             } else {
                 alert('Error: ' + response.statusText);
@@ -18,10 +18,17 @@ var getWeather = function () {
         });
 };
 
-var displayForecast = function(forecast) {
-    console.log(data);
-    console.log(forecast);
+var displayForecast = function(data) {
+    // console.log(data);
+    // console.log(forecast);
+    for (i = 0; i < 10; i++) {
+    //console.log(data);
+    $('#cityTemp').text("Temperature: " + data.list[i].main.temp + " Fahrenheit");
+    $('#cityTitle').text(data.city.name);
+    $('#cityWind').text("Wind: " + data.list[i].wind.speed);
+    $('#cityHum').text("Humidity: " + data.list[i].main.humidity);
 
+}
 }
 
 $('#init').click(function (event) {
